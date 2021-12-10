0b310c3f28f5758b46fb667ad20881b53218338d
Anan014 <ananamer95@gmail.com>
2021-12-10 14:40:14 +0200

    first commit

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 //update user
 router.put("/:id", async(req, res) => {
